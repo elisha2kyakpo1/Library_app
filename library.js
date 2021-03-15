6a44318d0b1e6495c78106e125d29c75085718fc
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-03-15 14:11:24 +0200

    Add read status

--- library.js
+++ library.js
@@ -1,14 +1,32 @@
 class Book {
-  constructor(title, author, pages){
+  constructor(title, author, pages, status){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.status = status;
   }
 }
 
 class Display {
   static displayBooks() {
-    const Data = StoreBooks.getBooks();
+    const Data = [
+      { title: "The king",
+       author: "Big bob",
+       pages: '400',
+       status: "read"
+      },
+      {
+        title: "The web",
+        author: "Untue",
+        pages: '2000',
+        status: "not read",
+      },
+      { title: "Captian in the ship",
+       author: "Elisha",
+       pages: '1200',
+       status: "read" 
+      }
+    ];
 
     Data.forEach((book) => Display.addBookToLibrary(book));
   }
@@ -18,9 +36,11 @@ class Display {
     const row = document.createElement('tr');
 
     row.innerHTML = `
+
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.pages}</td>
+      <td><a href="#" class="btn btn-success">${book.status}</a></td>
       <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
     `;
     list.appendChild(row);
@@ -49,45 +69,8 @@ class Display {
   }
 }
 
-class StoreBooks {
-  static getBooks(){
-    let books;
-    if(localStorage.getItem('books') === null){
-      books = [];
-    }else{
-      books.JSON.parse(localStorage.getItem('books'));
-    }
-    return books;
-  }
-
-  static addBook(book){
-    const books = StoreBooks.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(storedBook){
-    const books = StoreBooks.getBooks();
-    books.forEach((book, index) => {
-      if(book.storedBook === storedBook){
-        books.splice(index, 1);
-      }
-    });
-    localStorage.getItem('books', JSON.stringify(books));
-  }
-}
-
 document.addEventListener('DOMContentLoaded', Display.displayBooks);
 
-btnNode.addEventListener("click", () => {
-  if(btnNode.innerHTML.split(" ")[1] === "Yes") {
-      btnNode.style.backgroundColor = "Red"
-      btnNode.innerText = "Read: No"
-  } else {
-      btnNode.style.backgroundColor = "limegreen"
-      btnNode.innerText = "Read: Yes"
-  }
-})
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
@@ -95,20 +78,20 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   const pages = document.querySelector('#pages').value;
+  const status = document.querySelector('#status').value;
 
   if(title === '' || author === '' || pages === ''){
     Display.alert('please fill in all fields', 'danger');
   }
   else{
-    const book = new Book(title, author, pages);
+    const book = new Book(title, author, pages, status);
     Display.addBookToLibrary(book);
     Display.clearFields();
     Display.deleteDook();
-    StoreBooks.addBook();
+    StoreBooks.addBook(book);
   }
 })
 
 document.querySelector('#book-list').addEventListener('click', (e) => {
   Display.deleteDook(e.target)
 });
-
