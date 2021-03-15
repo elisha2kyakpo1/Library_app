d448eafdad8a2e983f485f0d75b8325e4170b23a
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-03-15 15:49:14 +0200

    Fix Eslint errors

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 class Book {
-  constructor(title, author, pages, status){
+  constructor(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -7,70 +7,69 @@ class Book {
   }
 }
 
-class Display {
-  static displayBooks() {
-    const Data = [
-      { title: "The king",
-       author: "Big bob",
-       pages: '400',
-       status: "read"
-      },
-      {
-        title: "The web",
-        author: "Untue",
-        pages: '2000',
-        status: "not read",
-      },
-      { title: "Captian in the ship",
-       author: "Elisha",
-       pages: '1200',
-       status: "read" 
-      }
-    ];
+function addBookToLibrary(book) {
+  const list = document.querySelector('#book-list');
+  const row = document.createElement('tr');
 
-    Data.forEach((book) => Display.addBookToLibrary(book));
-  }
+  row.innerHTML = `
 
-  static addBookToLibrary(book) {
-    const list = document.querySelector('#book-list');
-    const row = document.createElement('tr');
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td><a href="#" class="btn btn-success">${book.status}</a></td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+  `;
+  list.appendChild(row);
+}
 
-    row.innerHTML = `
+function Display() {
+  const Data = [
+    {
+      title: 'The king',
+      author: 'Big bob',
+      pages: '400',
+      status: 'read',
+    },
+    {
+      title: 'The web',
+      author: 'Untue',
+      pages: '2000',
+      status: 'not read',
+    },
+    {
+      title: 'Captian in the ship',
+      author: 'Elisha',
+      pages: '1200',
+      status: 'read',
+    },
+  ];
 
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.pages}</td>
-      <td><a href="#" class="btn btn-success">${book.status}</a></td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
-    `;
-    list.appendChild(row);
-  }
+  Data.forEach((book) => addBookToLibrary(book));
+}
 
-  static deleteDook(el){
-    if(el.classList.contains('delete')){
-      el.parentElement.parentElement.remove();
-    }
-  }
-
-  static alert(message, className){
-    const div = document.createElement('div');
-    div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.container');
-    const form = document.querySelector('#book-form');
-    container.insertBefore(div, form);
-    setTimeout(() => document.querySelector('.alert').remove(), 3000);
-  }
-
-  static clearFields(){
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
-    document.querySelector('#pages').value = '';
+function deleteBook(el) {
+  if (el.classList.contains('delete')) {
+    el.parentElement.parentElement.remove();
   }
 }
 
-document.addEventListener('DOMContentLoaded', Display.displayBooks);
+function alert(message, className) {
+  const div = document.createElement('div');
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  container.insertBefore(div, form);
+  setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
 
+function clearFields() {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  document.querySelector('#pages').value = '';
+}
+
+document.addEventListener('DOMContentLoaded', Display.displayBooks);
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
@@ -80,18 +79,16 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const pages = document.querySelector('#pages').value;
   const status = document.querySelector('#status').value;
 
-  if(title === '' || author === '' || pages === ''){
-    Display.alert('please fill in all fields', 'danger');
-  }
-  else{
+  if (title === '' || author === '' || pages === '') {
+    alert('please fill in all fields', 'danger');
+  } else {
     const book = new Book(title, author, pages, status);
-    Display.addBookToLibrary(book);
-    Display.clearFields();
-    Display.deleteDook();
-    StoreBooks.addBook(book);
+    addBookToLibrary(book);
+    clearFields();
+    deleteBook();
   }
-})
+});
 
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  Display.deleteDook(e.target)
+  deleteBook(e.target);
 });
